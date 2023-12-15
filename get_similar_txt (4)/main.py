712b6bee3cba7dab8307122c9dbd04459e509203
nachikethsj <nachiketh.s.j@live.com>
2023-12-15 22:28:30 +0530

    push

--- get_similar_txt (4)/main.py
+++ get_similar_txt (4)/main.py
@@ -55,6 +55,6 @@ async def root(request_body:Query_url):
         print("---error----",str(err))
         return {"statusCode":500,"message": "Internal Server error"}
     
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="Chatbot"), name="static")
 if __name__=="__main__":
     uvicorn.run("main:app",host='0.0.0.0', port=3000, reload=True, workers=3)
